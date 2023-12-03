63e2d1bfeb6a6f58698ca93f56c28753bbe30638
gbengailesanmi <145447898+gbengailesanmi@users.noreply.github.com>
2023-12-03 19:49:36 +0000

    update-1

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// const txt1 = document.getElementById('discription');
-// const btn1 = document.getElementById('button1');
-// const out1 = document.getElementById('output1');
+const txt1 = document.getElementById('discription');
+const btn1 = document.getElementById('button1');
+const out1 = document.getElementById('output1');
 
-// function func1(event){
-//   event.preventDefault();
-//   out1.innerHTML = txt1.value;
-// }
-// btn1.addEventListener('click',func1);
+function func1(event){
+  event.preventDefault();
+  out1.innerHTML = txt1.value;
+}
+btn1.addEventListener('click',func1);
 
 
 
-// // const btn2 = document.getElementById('button2')
-// const imgInput = document.querySelector("#photo1");
-// var uploaded_img = " ";
+// const btn2 = document.getElementById('button2')
+const imgInput = document.querySelector("#photo1");
+var uploaded_img = " ";
 
-// imgInput.addEventListener("change",function(){
-//   const reader = new FileReader();
-//   reader.addEventListener("load", ()=>{
-//     uploaded_img = reader.result;
-//     document.querySelector("#canvas").style.backgroundImage = "url("+uploaded_img+")";
-//   });
-//   reader.readAsDataURL(this.files[0]);
-// })
+imgInput.addEventListener("change",function(){
+  const reader = new FileReader();
+  reader.addEventListener("load", ()=>{
+    uploaded_img = reader.result;
+    document.querySelector("#canvas").style.backgroundImage = "url("+uploaded_img+")";
+  });
+  reader.readAsDataURL(this.files[0]);
+})
