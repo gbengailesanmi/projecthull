16c2f0f8e9fd2442c0b525ffb28fa8fb9c11148f
gbengailesanmi <145447898+gbengailesanmi@users.noreply.github.com>
2024-03-30 15:51:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
   const imageForm = document.getElementById('imageForm');
@@ -44,12 +55,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
