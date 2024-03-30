495600e3dbc31522767c547484ca5a797f4f66ae
gbengailesanmi <145447898+gbengailesanmi@users.noreply.github.com>
2024-03-30 12:57:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
       formData.append('file', imgInput.files[0]);
 
       // Replace 'API_ENDPOINT' with the actual endpoint
-      fetch('16.170.228.246:8000/p', { 
+      fetch('http://16.170.228.246:8000/p', { 
           method: "POST",
           body: formData
       })
