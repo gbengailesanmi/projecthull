8f5eb87e39fc16321640fbebedb54124aafd5a1a
gbengailesanmi <145447898+gbengailesanmi@users.noreply.github.com>
2024-03-30 08:39:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
       formData.append('file', imgInput.files[0]);
 
       // Replace 'API_ENDPOINT' with the actual endpoint
-      fetch('http://ec2-51-21-140-176.eu-north-1.compute.amazonaws.com/colourizer/', { 
+      fetch('https://ec2-13-51-85-212.eu-north-1.compute.amazonaws.com/colourizer/', { 
           method: 'POST',
           body: formData
       })
